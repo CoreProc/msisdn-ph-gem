901f07fcc8d304f3fea2b5ed6680a07fdfff8320
Jameson Candava <candavajameson@gmail.com>
2015-08-27 13:18:50 +0800

    add rspec test

--- spec/msisdn_spec.rb
+++ spec/msisdn_spec.rb
@@ -1,6 +1,19 @@
-require 'spec_helper'
+require 'Msisdn'
 
-describe Msisdn do
+RSpec.describe MsisdnPH::Msisdn do
 
+	describe "validate" do
+		it "returns true if valid mobile number" do
+			expect(MsisdnPH::Msisdn.validate("09171231234")).to eq true
+		end
+	end
+
+	describe "operator" do
+		it "returns the operator of the mobile number" do
+			msisdn = MsisdnPH::Msisdn.new "09171231234"
+
+			expect(msisdn.operator).to eq "GLOBE"
+		end
+	end
 
 end
