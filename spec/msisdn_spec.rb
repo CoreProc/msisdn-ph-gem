e5227cbcf097efd6d05b772adaf430083a81a0b3
Jameson Candava <candavajameson@gmail.com>
2015-08-27 19:19:55 +0800

    added new rspec test

--- spec/msisdn_spec.rb
+++ spec/msisdn_spec.rb
@@ -6,13 +6,32 @@ RSpec.describe MsisdnPH::Msisdn do
 		it "returns true if valid mobile number" do
 			expect(MsisdnPH::Msisdn.validate("09171231234")).to eq true
 		end
+
+		it "returns false if invalid mobile number" do
+			expect(MsisdnPH::Msisdn.validate("0918123123")).to eq false
+		end
 	end
 
 	describe "operator" do
-		it "returns the operator of the mobile number" do
-			msisdn = MsisdnPH::Msisdn.new "09171231234"
+		it "returns the correct operator" do
+			
+			globeMsisdn = MsisdnPH::Msisdn.new "09171231234"
 
-			expect(msisdn.operator).to eq "GLOBE"
+			smartMsisdn = MsisdnPH::Msisdn.new "09191231234"
+
+			sunMsisdn = MsisdnPH::Msisdn.new "09321231234"
+
+			unknownMsisdn = MsisdnPH::Msisdn.new "09511231234"
+
+
+			expect(globeMsisdn.operator).to eq "GLOBE"
+
+			expect(smartMsisdn.operator).to eq "SMART"
+
+			expect(sunMsisdn.operator).to eq "SUN"
+
+			expect(unknownMsisdn.operator).to eq "UNKNOWN"
+
 		end
 	end
 
